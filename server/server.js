d9fc8965da9176e4e682faadadc2ec6717323669
nachomirsol <nachomirsol@gmail.com>
2018-06-15 09:06:19 +0200

    get/post/put/delete queries and body-parser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const app = express();
 
-app.get('/',(req,res) => {
+const bodyParser = require('body-parser');
 
-	res.json('Hello World');
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+
+//parse application/json middlewares cada petición pasa por estas lineas
+app.use(bodyParser.json());
+
+
+app.get('/usuario',(req,res) => {
+
+	res.json('get Usuario');
+
+});
+
+
+app.post('/usuario:id', (req,res) => {
+	
+	let body = req.body;
+
+	if(body.name === undefined){
+
+		res.status(400).json({
+			ok:false,
+			message:'name is required',
+		});
+	}
+
+	res.json({
+		user:body
+	});
+
+});
+
+
+app.put('/usuario',(req,res) => {
+
+	let id = req.params.id;
+
+	res.json({
+		id
+	});
+
+});
+
+
+app.delete('/usuario')
 
 app.listen(3000, () => {
 	console.log('Escuchando el puerto: ', 3000);
