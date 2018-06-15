838598ce740f6633c28211350ebb8b1e3421d756
nachomirsol <nachomirsol@gmail.com>
2018-06-15 10:05:02 +0200

    formated file

--- server/server.js
+++ server/server.js
@@ -5,39 +5,39 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //parse application/json middlewares cada petición pasa por estas lineas
 app.use(bodyParser.json());
 
 
-app.get('/usuario',(req,res) => {
+app.get('/usuario', (req, res) => {
 
 	res.json('get Usuario');
 
 });
 
 
-app.post('/usuario:id', (req,res) => {
-	
+app.post('/usuario:id', (req, res) => {
+
 	let body = req.body;
 
-	if(body.name === undefined){
+	if (body.name === undefined) {
 
 		res.status(400).json({
-			ok:false,
-			message:'name is required',
+			ok: false,
+			message: 'name is required',
 		});
 	}
 
 	res.json({
-		user:body
+		user: body
 	});
 
 });
 
 
-app.put('/usuario',(req,res) => {
+app.put('/usuario', (req, res) => {
 
 	let id = req.params.id;
 
@@ -48,10 +48,10 @@ app.put('/usuario',(req,res) => {
 });
 
 
-app.delete('/usuario', (req,res) => {
+app.delete('/usuario', (req, res) => {
 
 	res.json({
-		
+
 	});
 })
 
