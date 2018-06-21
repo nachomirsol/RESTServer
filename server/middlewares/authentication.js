dbb0cc621ccad931bb980f48706a94e54c495f6b
nachomirsol <nachomirsol@gmail.com>
2018-06-21 09:10:31 +0200

    middlewares for token

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+// =======================
+// Verify token
+// =======================
+let verifyToken = (req,res,next) => {
+
+	let token = req.get('token'); 
+	
+	jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+		if(err){
+
+			return res.status(401).json({
+				ok:false,
+				err
+			});
+		}
+
+		req.user = decoded.user;
+		next();
+
+	});
+
+	
+
+}
+
+
+module.exports = {
+	verifyToken
+}
